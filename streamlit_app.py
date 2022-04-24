8ee2866363d5982b62f2c61f148fa7f422546298
Klaudia Ciochoń <104318146+KlaudiaKoldarz1@users.noreply.github.com>
2022-04-24 19:48:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title('My Mom\`s New Healthy Diner')
+st.header('Breakfast Menu')
+st.Text('Omega 3 & Blueberry OatMeal')
+st.Text('Kale, Spinach & Rocket Smoothie')
+st.Txt('Hard-Boiled Free-Range Egg')
