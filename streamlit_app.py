1a26811978cd8a85489220480b17a18c754ad270
Klaudia Ciochoń <104318146+KlaudiaKoldarz1@users.noreply.github.com>
2022-05-14 15:45:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header('Fruityvice Fruit Advice!')
-fruit_choice = st.text_imput('What friut would you like information aobut?', 'Kiwi')
+fruit_choice = st.text_input('What friut would you like information aobut?', 'Kiwi')
 #st.write('The user entered', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 
