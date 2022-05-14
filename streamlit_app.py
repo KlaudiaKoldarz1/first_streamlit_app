5968cdcd88427ddf90c5082502248b6ae863d9c0
Klaudia Ciochoń <104318146+KlaudiaKoldarz1@users.noreply.github.com>
2022-05-14 15:44:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.dataframe(fruits_to_show)
 
 st.header('Fruityvice Fruit Advice!')
 fruit_choice = st.text_imput('What friut would you like information aobut?', 'Kiwi')
-st.write.('The user entered', fruit_choice)
+st.write('The user entered', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 
 #st.text(fruityvice_response.json())
