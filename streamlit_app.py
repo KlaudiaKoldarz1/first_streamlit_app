89166d5fd5323cf34854869164cc831139642c6d
Klaudia Ciochoń <104318146+KlaudiaKoldarz1@users.noreply.github.com>
2022-05-16 22:50:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,14 +34,21 @@ except URLError as e:
   st.Error()
 #st.text(fruityvice_response.json())
 
-st.stop()
+#st.stop()
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
+ 
+
+
 st.header("The friut list contains:")
-st.dataframe(my_data_rows)
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+    return  my_cur.fetchall()
+
+if st.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    st.dataframe(my_data_rows)
 
 
 fruit_choice1 = st.text_input('What friut would you like to add?', 'Kiwi')
